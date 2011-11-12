20f294f8aa3c73fe0af15d3484af2219c52fc0b4
Fabio Anderegg <fotcorn@gmail.com>
2011-11-12 21:05:03 +0100

    instruction set with argument size

--- instr.py
+++ instr.py
@@ -1,17 +1,29 @@
 from instruction_set import instruction_set
 
 
+zero_arg_size = ['A', 'impl']
+one_arg_size = ['#', 'ind', 'X,ind', 'ind,Y', 'rel', 'zpg', 'zpg,X', 'zpg,Y']
+two_arg_size = ['abs', 'abs,X', 'abs,Y']
+
+
 for id, instr in instruction_set.items():
-    instr = instr[0].split(" ")
     line = "instruction_set[" + hex(id) + "] = ('"
-    line += instr[0] + "','" + instr[1] + "')"
+    line += instr[0] + "', '" + instr[1] + "'"
+    
+    if instr[1] in zero_arg_size:
+        size = 0
+    if instr[1] in one_arg_size:
+        size = 1
+    if instr[1] in two_arg_size:
+        size = 2
+    
+    line += ", " + str(size) + ")"
     
     if instr[0] != '???':
         print line
         
         
 for id, instr in instruction_set.items():
-    instr = instr[0].split(" ")
     line = "instruction_set[" + hex(id) + "] = ('"
     line += instr[0] + "','" + instr[1] + "')"
     
