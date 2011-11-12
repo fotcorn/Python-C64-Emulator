8bc6e2f5d127332e1673ed57c7f0d257fac3ae24
Fabio Anderegg <fotcorn@gmail.com>
2011-11-13 00:29:46 +0100

    iteration 3

--- cpu.py
+++ cpu.py
@@ -41,37 +41,105 @@ class CPU():
     def run(self, address):
         self.PC.set(address)
         
-        
         while True:
             instr = self.fetch()
-            if instr[0] == 'BRK':
+            self.PC.inc()
+            if instr[1] == 'BRK':
                 return
             self.execute(instr)
         
         
     def fetch(self):
         data = self.memory.read(self.PC.get())
-        instr = instruction_set[data]
-        instr[0]
+        mnem, arg_type, size = instruction_set[data]
+        instruction = {'mnem' : mnem, 'arg' : arg_type}
         
-        if instr[2] == 0:
-            ret = (instr[0], instr[1])
-        elif instr[2] == 1:
+        # fetch instruction with argument
+        if size == 1:
             self.PC.inc()
-            ret = (instr[0], instr[1], self.memory.read(self.PC.get()))
-        elif instr[2] == 2:
+            data = self.memory.read(self.PC.get())
+        elif size == 2:
             self.PC.inc()
             val1 = self.memory.read(self.PC.get())
             self.PC.inc()
             val2 = self.memory.read(self.PC.get())
-            ret = (instr[0], instr[1], val2*256+val1)
-        self.PC.inc()
-        return ret
-    
-    def execute(self, instr):
-        print instr
-    
+            data = val2*256+val1
         
+        # resolve arguments to real address or value
+        if arg_type == 'A':
+            instruction['value'] = self.A.get()
+        elif 'abs' in arg_type:
+            if 'X' in arg_type:
+                instruction['address'] = data + self.X.get()
+            elif 'Y' in arg_type:
+                instruction['address'] = data + self.Y.get()
+            else:
+                instruction['address'] = data
+        elif arg_type == '#':
+            instruction['value'] = data
+        elif arg_type == 'ind':
+            instruction['address'] = self.memory.get((data+1)*256) + self.memory.get(data)
+        elif 'ind' in arg_type:
+# TODO: implement indirect adressing 
+            if arg_type == 'X,ind':
+                pass
+            else: # ind,y
+                pass
+        elif arg_type == 'rel':
+            if data >> 7:
+                instruction['address'] = self.PC.get() - data - 128 
+            else:
+                instruction['address'] = self.PC.get() + data
+        elif 'zpg' in arg_type:
+            if arg_type == 'zpg,X':
+                instruction['address'] = data + self.X.get()
+            if arg_type == 'zpg,Y':
+                instruction['address'] = data + self.Y.get()
+            else:
+                instruction['address'] = data
+        
+        return instruction
+        
+    def execute(self, instr):
+        # prepare
+        mnem = instr['mnem']
+        arg_type = instr['arg_type']
+        if 'value' in instr:
+            value = instr['value']
+        if 'address' in instr:
+            address = instr['address']
+        
+        # comands
+        if mnem == 'LDY':
+            self.Y.set(value)
+        elif mnem == 'TYA':
+            self.A.set(self.Y.get())
+        elif mnem == 'CLC':
+            self.SR.C = False
+        elif mnem == 'ADC':
+            pass
+        elif mnem == 'JSR':
+            if address == 0xFFD2:
+                self.console.putc(self.A.get())
+        elif mnem == 'CMP':
+            res = self.A.get() - value
+            if res < 0:
+                self.SR.N = True
+            else:
+                self.SR.N = False
+            if res == 0:
+                self.SR.Z = True
+            else:
+                self.SR.Z = False
+        elif mnem == 'BEQ':
+            if self.SR.Z:
+                self.PC.set(address)
+        elif mnem == 'DEY':
+            self.Y.dec()
+        elif mnem == 'JMP':
+            self.PC.set(address)
+        elif mnem == 'RTS':
+            pass
         
 
         
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import cpu
 import memory
 import programloader
+import console
 
 if __name__ == "__main__":
     cpu = cpu.CPU()
     memory = memory.Memory()
+    console = console.Console()
+    
     cpu.memory = memory
+    cpu.console = console
     
     loader = programloader.ProgramLoader(memory)
     loader.load_file('/home/corn/Projekte/6502emu/examples/asm/count10')
