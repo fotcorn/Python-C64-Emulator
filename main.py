5e81dff1644b80b02d2c0770290c4a387bae9698
fotcorn <fotcorn@gmail.com>
2015-03-28 23:14:00 +0100

    load programs from command line argument

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
+import sys
+
 import cpu
 import memory
 import programloader
 import console
 
+
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print 'Usage:'
+        print './main.py <program>'
+        sys.exit(1)
+
     cpu = cpu.CPU()
     memory = memory.Memory()
     console = console.Console()
@@ -12,6 +21,6 @@ if __name__ == "__main__":
     cpu.console = console
     
     loader = programloader.ProgramLoader(memory)
-    loader.load_file('/home/corn/Projekte/6502emu/examples/conditional_jumps/task1')
+    loader.load_file(sys.argv[1])
     
     cpu.run(0xC000)
